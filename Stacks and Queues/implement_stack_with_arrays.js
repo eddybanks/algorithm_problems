e316726444188166466790a5808ccb5f0001918d
Edwin Agbenyega <eddy.devv@gmail.com>
2019-08-17 16:49:55 -0600

    Implement stack with arrays

--- Stacks and Queues/implement_stack_with_arrays.js
+++ Stacks and Queues/implement_stack_with_arrays.js
@@ -1,26 +1,15 @@
 class Stack {
   constructor() {
     this.array = []
-    this.top = null
-    this.bottom = null
-    this.length = 0
   }
 
   push(value) {
     this.array.push(value)
-    this.top = this.array[this.array.length - 1]
-    this.bottom = this.array[0]
-    this.length++
     return this
   }
 
   pop() {
-    if(this.top === this.bottom) {
-      this.bottom = null
-    }
     this.array.pop()
-    this.top = this.array[this.array.length - 1]
-    this.length--
     return this
   }
 
