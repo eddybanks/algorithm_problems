bd24e33c46b5a26fb06e76a6b214cea601fc78b0
Edwin Agbenyega <eddybanks03@gmail.com>
2019-08-27 16:10:46 -0600

    Add ZTM implementation of insertion sort - need to revisit this algorithm

--- Sorting/insertion_sort.js
+++ Sorting/insertion_sort.js
@@ -18,5 +18,24 @@ function insertionSort(array) {
   }
 }
 
-insertionSort(numbers)
+// ZTM Version of insertion sort
+function ztmInsertionSort(array) {
+  const length = array.length
+  for(let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      // move number to the first position
+      array.unshift(array.splice(i,1)[0])
+    } else {
+      // find where number should go
+      for(let j = 1; j < i; j++) {
+        if(array[i] > array[j-1] && array[i] < array[j]) {
+          array.splice(j,0,array.splice(i,1)[0])
+        }
+      }
+    }
+  }
+}
+
+// insertionSort(numbers)
+ztmInsertionSort(numbers)
 console.log(numbers)
