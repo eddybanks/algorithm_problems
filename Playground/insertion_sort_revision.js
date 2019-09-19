69b8a4624728aefb417e2578039aca0ad3b59fa3
Edwin Agbenyega <eddybanks03@gmail.com>
2019-09-19 16:02:14 -0600

    Just a few lines in and a bit stuck :(

--- Playground/insertion_sort_revision.js
+++ Playground/insertion_sort_revision.js
@@ -1,7 +1,13 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function insertionSort(array) {
-  
+  let tmp = array[0]
+  let j = 0
+  for(let i = 1; i < array.length; i++) {
+    while(array[i] < array[j]) {
+      j++
+    }
+  }
 }
 
 insertionSort(numbers);
