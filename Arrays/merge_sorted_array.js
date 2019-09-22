15e6c5ceeba6c65cd5b39cca6dedef681dc09b42
Edwin Agbenyega <eddy.devv@gmail.com>
2019-09-22 11:22:40 -0600

    not sure

--- Arrays/merge_sorted_array.js
+++ Arrays/merge_sorted_array.js
@@ -58,9 +58,27 @@ var merge2 = function(nums1, m, nums2, n) {
   return nums1
 }
 
+var merge3 = function(nums1, m, nums2, n) {
+
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (k >= 0) {
+      if (k === i) {
+          return;
+      } else {
+          nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+      }
+  }
+
+  return nums1
+};
+
 const nums1 = [1,2,3,0,0,0]
 const m = 3
 const nums2 = [2,5,6]
 const n = 3
 
-console.log(merge2([0], 0, [1], 1))
+merge3(nums1, m, nums2, n)
+console.log(nums1)
