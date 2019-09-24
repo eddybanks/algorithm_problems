5f76739159052c946df104cbb133fc6a5e840ddf
Edwin Agbenyega <eddy.devv@gmail.com>
2019-09-23 21:32:51 -0600

    Complete bubble sort revision in playground

--- Playground/bubble_sort_revision.js
+++ Playground/bubble_sort_revision.js
@@ -1,7 +1,18 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
 function bubbleSort(array) {
-
+  let len = array.length
+  while(len > 1) {
+    let tmp = array[0]
+    for(let i = 1; i < len; i++) {
+      if(array[i-1] > array[i]) {
+        tmp = array[i-1]
+        array[i-1] = array[i]
+        array[i] = tmp
+      }
+    }
+    len--
+  }
 }
 
 bubbleSort(numbers)
