b23b92e91bc81a76869e95a7101a68c765198bec
Edwin Agbenyega <eddybanks03@gmail.com>
2019-09-24 16:12:51 -0600

    set up template for selection sort revision in playground

--- Playground/selection_sort_revision.js
+++ Playground/selection_sort_revision.js
@@ -1,7 +1,20 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
 function selectionSort(array) {
-
+  let j = 0
+  let len = array.length
+  while(j < len) {
+    let min = j
+    for(let i = j+1; i < len; i++) {
+      if(array[i] < min) {
+        min = i
+      }
+    }
+    for(let k = j; k < min; k++) {
+      
+    }
+    j++
+  }
 }
 
 selectionSort(numbers)
