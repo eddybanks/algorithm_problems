1e2f3be526b12543e8e5a8b4acb8051dd8d2de15
Edwin Agbenyega <eddybanks03@gmail.com>
2019-09-25 16:07:14 -0600

    implement selection sort revision in playground but with a few problems

--- Playground/selection_sort_revision.js
+++ Playground/selection_sort_revision.js
@@ -1,19 +1,22 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
 function selectionSort(array) {
-  let j = 0
-  let len = array.length
-  while(j < len) {
-    let min = j
-    for(let i = j+1; i < len; i++) {
+  min = array[0]
+  first = 1
+  while(first < array.length){
+    tmp = first - 1
+    min = array[first]
+    for(let i = first; i < array.length; i++) {
       if(array[i] < min) {
-        min = i
+        min = array[i]
+        tmp = i
       }
     }
-    for(let k = j; k < min; k++) {
-      
+    for(let j = tmp; j >= first; j--) {
+      array[j] = array[j-1]
     }
-    j++
+    array[first-1] = min
+    first++
   }
 }
 
