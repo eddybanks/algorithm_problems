ca0e57d3d81f07dc9e947a3dd99f0c52d16e16d7
Edwin Agbenyega <eddybanks03@gmail.com>
2021-12-16 20:45:57 -0800

    Practice fibonacci iterative and recursive

--- Playground/fibonacci.js
+++ Playground/fibonacci.js
@@ -1,15 +1,17 @@
 function fibonacciIterative(n) {
-  for (let i = 1; i < n; i++) {}
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
 }
 
 function fibonacciRecursive(n) {
-  if (n === 0) {
-    return 0;
-  }
-  if (n === 1) {
-    return 1;
+  if (n < 2) {
+    return n;
   }
   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
-console.log(fibonacciRecursive(6));
+console.log(fibonacciRecursive(8));
+console.log(fibonacciIterative(8));
